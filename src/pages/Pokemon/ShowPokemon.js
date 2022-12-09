3ef5bc2eddd9bcb1cf6befde9f0025e662dc41c9
saefulbarkah <saefulbarkah29062003@gmail.com>
2022-12-09 17:56:50 +0700

    fix element color missing

--- src/pages/Pokemon/ShowPokemon.js
+++ src/pages/Pokemon/ShowPokemon.js
@@ -10,7 +10,6 @@ import { BsFillCaretLeftFill } from "react-icons/bs";
 import Dropdown from "../../components/Dropdown";
 import DropdownItem from "../../components/DropdownItem";
 import ChangeColorNumber from "../../components/ChangeColorNumber";
-import elementColor from "../../elementColor.json";
 
 function ShowPokemon() {
   const [pokemon, setPokemon] = useState([]);
@@ -19,6 +18,24 @@ function ShowPokemon() {
   const [stat, setStat] = useState([]);
   const [abilityEffect, setAbilityEffect] = useState([]);
   const { name } = useParams();
+  const elementColor = [
+    { element: "fire", color: "bg-red-600" },
+    { element: "grass", color: "bg-green-600" },
+    { element: "poison", color: "bg-purple-800" },
+    { element: "electric", color: "bg-yellow-600" },
+    { element: "fairy", color: "bg-pink-700" },
+    { element: "water", color: "bg-blue-700" },
+    { element: "ice", color: "bg-sky-500" },
+    { element: "fighting", color: "bg-orange-700" },
+    { element: "flying", color: "bg-gray-500" },
+    { element: "bug", color: "bg-green-800" },
+    { element: "dark", color: "bg-slate-800" },
+    { element: "normal", color: "bg-purple-500" },
+    { element: "ghost", color: "bg-blue-700" },
+    { element: "ground", color: "bg-orange-700" },
+    { element: "rock", color: "bg-orange-900" },
+    { element: "steel", color: "bg-lime-700" },
+  ];
 
   useEffect(() => {
     async function showPoke() {
@@ -124,9 +141,9 @@ function ShowPokemon() {
                         type.map((i) =>
                           data.element === i["type"].name ? (
                             <p
-                              className={`rounded-lg capitalize  ${
+                              className={`rounded-lg capitalize ${
                                 data.element === i["type"].name && data.color
-                              } ${data.fontColor}`}
+                              }`}
                               key={key}
                             >
                               {i["type"].name}
